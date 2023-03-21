d059da3e7ba9923bf979be0d2a75d9ad98bc2a6b
FunnyCPP <kiienko.romaniuk@gmail.com>
2023-03-21 21:03:13 +0100

    Added slider and component with 3 columns to index.html Created list.html with list in html Created paragraphs.html Updated style.css amd created script.js Added images to project

---
+++ script.js
@@ -0,0 +1,25 @@
+const mobileNav = document.querySelector('ul');
+const burgerIcon = document.querySelector('.burger');
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sliderItems = document.querySelectorAll('.slider-item');
+    let currentItem = 0;
+    const animationDuration = 3000; // 3 seconds
+
+    function changeItem() {
+        sliderItems[currentItem].classList.remove('active');
+        currentItem++;
+        if (currentItem >= sliderItems.length) {
+            currentItem = 0;
+        }
+        sliderItems[currentItem].classList.add('active');
+    }
+
+    setInterval(changeItem, animationDuration);
+});
+
+burgerIcon.addEventListener('click', function () {
+    mobileNav.classList.toggle('active');
+    burgerIcon.classList.toggle('active');
+
+})
